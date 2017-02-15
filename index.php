c10c68c60e4a2698ebe260544a856227a835c33a
Hector Benjamin <hector_benjas@hotmail.com>
2017-02-15 17:20:11 -0500

    Commit2

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+	Autor: Hector Benjamin Pech Peraza
+	Version: 1.1
+	Fecha:15/02/2017
+*/
 define('DIR', 'http://localhost:8080/5ToCuatri/MiAppGestion');
 include_once("Controlador/Controlador.php");
  ?>
